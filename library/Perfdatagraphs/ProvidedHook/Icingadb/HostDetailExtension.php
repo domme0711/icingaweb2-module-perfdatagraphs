2794e1592f6f091d71458e7a31fb57451b4c2a66
Markus Opolka <markus.opolka@netways.de>
2025-02-11 16:25:28 +0100

    Dry Refactor

--- library/Perfdatagraphs/ProvidedHook/Icingadb/HostDetailExtension.php
+++ library/Perfdatagraphs/ProvidedHook/Icingadb/HostDetailExtension.php
@@ -2,7 +2,8 @@
 
 namespace Icinga\Module\Perfdatagraphs\ProvidedHook\Icingadb;
 
-use Icinga\Module\Perfdatagraphs\Icingadb\PerfdataChart;
+use Icinga\Module\Perfdatagraphs\Common\PerfdataChart;
+use Icinga\Module\Perfdatagraphs\Icingadb\CustomVarsHelper;
 
 use Icinga\Module\Icingadb\Hook\HostDetailExtensionHook;
 use Icinga\Module\Icingadb\Model\Host;
@@ -22,8 +23,20 @@ class HostDetailExtension extends HostDetailExtensionHook
      */
     public function getHtmlForObject(Host $host): ValidHtml
     {
+        $serviceName = $host->checkcommand_name ?? '';
+        $hostName = $host->name ?? '';
+        $checkCommandName = $host->checkcommand_name ?? '';
+
+        $cvh = new CustomVarsHelper();
+        $customvars = $cvh->getPerfdataGraphsConfigForObject($host);
+
+        // Check if charts are disabled for this object, if so we just return.
+        if ($customvars[$cvh::CUSTOM_VAR_CONFIG_DISABLE] ?? false) {
+            return HtmlString::create('');
+        }
+
         // Get the configured element for the host.
-        $chart = $this->createChart($host);
+        $chart = $this->createChart($hostName, $serviceName, $checkCommandName);
 
         if (empty($chart)) {
             // Probably unecessary but just to be safe.
--- library/Perfdatagraphs/ProvidedHook/Icingadb/ServiceDetailExtension.php
+++ library/Perfdatagraphs/ProvidedHook/Icingadb/ServiceDetailExtension.php
@@ -2,7 +2,8 @@
 
 namespace Icinga\Module\Perfdatagraphs\ProvidedHook\Icingadb;
 
-use Icinga\Module\Perfdatagraphs\Icingadb\PerfdataChart;
+use Icinga\Module\Perfdatagraphs\Common\PerfdataChart;
+use Icinga\Module\Perfdatagraphs\Icingadb\CustomVarsHelper;
 
 use Icinga\Module\Icingadb\Hook\ServiceDetailExtensionHook;
 use Icinga\Module\Icingadb\Model\Service;
@@ -22,8 +23,20 @@ class ServiceDetailExtension extends ServiceDetailExtensionHook
      */
     public function getHtmlForObject(Service $service): ValidHtml
     {
+        $serviceName = $service->name ?? '';
+        $hostName = $service->host->name ?? '';
+        $checkCommandName = $service->checkcommand_name ?? '';
+
+        $cvh = new CustomVarsHelper();
+        $customvars = $cvh->getPerfdataGraphsConfigForObject($service);
+
+        // Check if charts are disabled for this object, if so we just return.
+        if ($customvars[$cvh::CUSTOM_VAR_CONFIG_DISABLE] ?? false) {
+            return HtmlString::create('');
+        }
+
         // Get the configured element for the service.
-        $chart = $this->createChart($service);
+        $chart = $this->createChart($hostName, $serviceName, $checkCommandName);
 
         if (empty($chart)) {
             // Probably unecessary but just to be safe.
