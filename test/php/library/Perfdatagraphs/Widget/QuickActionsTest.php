c4e345b7d14edc86024d7b60c3a3b92ede97e19e
Markus Opolka <markus.opolka@netways.de>
2025-02-11 12:57:22 +0100

    Extend tests

---
+++ test/php/library/Perfdatagraphs/Widget/QuickActionsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Icinga\Module\Perfdatagraphs;
+
+use Icinga\Module\Perfdatagraphs\Widget\QuickActions;
+
+use PHPUnit\Framework\TestCase;
+
+final class QuickActionsTest extends TestCase
+{
+    public function test_assemble()
+    {
+
+        $qa = new QuickActions();
+
+        $this->assertStringContainsString("data-duration=", $qa->render());
+    }
+}
