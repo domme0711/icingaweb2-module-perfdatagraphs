ed46889b4ddde7070c1023720da09f177c49008e
Markus Opolka <markus.opolka@netways.de>
2026-07-09 11:31:28 +0200

    Fix sort breaking the highlight feature
    There is some rendundancy here, but I did not want to introduce a
    new method on the Model (yet).

--- library/Perfdatagraphs/Common/PerfdataChart.php
+++ library/Perfdatagraphs/Common/PerfdataChart.php
@@ -111,13 +111,7 @@ trait PerfdataChart
 
         $datasets = [];
 
-        // Ensure labels have a predictable order
-        $sets = $response->getDatasets();
-        uasort($sets, function ($a, $b) {
-            return strnatcmp($a->getTitle(), $b->getTitle());
-        });
-
-        foreach ($sets as $dataset) {
+        foreach ($response->getDatasets() as $dataset) {
             // If the filter param is set, we only use the dataset when the label matches
             if (count($filter) > 0) {
                 if (!in_array($dataset->getTitle(), $filter)) {
